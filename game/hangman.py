4c675ee168bb82128fbb00c7810c9ddd8f11aa9e
Rock's Vishu <you@example.com>
2019-01-02 17:10:26 +0530

    more changes

--- game/hangman.py
+++ game/hangman.py
@@ -1,28 +1,37 @@
-import random
+import random,string
+
+def play_again():
+	play_again = input("Do you wnat to play again(y/n): ").lower()
+	if play_again == "y":
+		return hangman(words)
+	else:
+		return "Thanks for playing"
 
 def hangman(list_of_words):
 	random_word = random.choice(list_of_words)
-	secrate_word = random_word
-	available_letters = list("abcdefghijklmnopqrstuvwxyz")
+	secret_word = random_word
+	available_letters = list(string.ascii_lowercase)
 	answer = ''
-	replace = ("-" * len(secrate_word))
-	list2 = list(replace)
+	chances = 8
+	blank_spaces = ("-" * len(secret_word))
+	list2 = list(blank_spaces)
 	position_list = []
-	message = '''You can't guess same letter again,
-please guess letters from available letters list.'''
-	print("Welcome to Hangman!")
+	message = '''You can't guess same letter again,\nplease guess letters from available letters list.'''
+	print("Welcome to the game, Hangman!")
+	print("I am thinking of a word that is " + str(len(secret_word)) + " letters long.")
 	print("")
-	print(replace)
-	while True:
+	print(blank_spaces)
+	while chances > 0:
 		print("")
-		print("Available letters to guess")
+		print("You have " + str(chances) + " chances to guess the correct word.")
+		print("Available letters to guess:- ")
 		print(available_letters)
 		try:
 			guess = input("Guess your letter: ")
 			if len(guess) == 1:
 				position_list = []
-				if guess in secrate_word:
-					list1 = list(secrate_word)
+				if guess in secret_word:
+					list1 = list(secret_word)
 					counter = 0
 					while counter < len(list1):
 						if list1[counter] == guess:
@@ -40,22 +49,22 @@ please guess letters from available letters list.'''
 					print(answer)
 
 					available_letters.remove(guess)
+					chances -=1
 				else:
 					print("Incorrect Guess")
-				if answer == secrate_word:
+					chances -=1
+				if answer == secret_word:
 						print("you won the game")
-						play_again = input("Do you wnat to play again(y/n): ").lower()
-						if play_again == "y":
-							continue
-						else:
-							print("Thanks for playing")
-							break
+						break
 			else:
 				print("Please Enter one letter at time")
 		except:
 			print("")
 			print(message)
 
+	again = play_again()
+	print(again)
+		
 
 words = ['mango','apple','london','america','lion','tigher','onion','bus','car','happy','beautiful','ugly']
 hangman(words)
